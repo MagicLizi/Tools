4426254a18baae3bc7038feb40085ebfffe8f056
MagicLizi <liloveli0928@vip.qq.com>
2016-05-03 11:08:58 +0800

    first commit

---
+++ changeRGBToHex.js
@@ -0,0 +1,24 @@
+#!/usr/local/bin/node
+var arguments = process.argv;
+
+
+var red = arguments[2];
+var green = arguments[3];
+var blue = arguments[4];
+
+console.log("rgb:("+ red + "," + green + "," + blue +")");
+
+var redDivisor    = parseInt(red/16);
+var redRemainder  = parseInt(red%16);
+
+var greenDivisor  = parseInt(green/16);
+var greenRemainer = parseInt(green%16);
+
+var blueDivisor   = parseInt(blue/16);
+var blueRemainer  = parseInt(blue%16);
+
+var hex = redDivisor.toString(16).toLocaleUpperCase() + redRemainder.toString(16).toLocaleUpperCase() +
+          greenDivisor.toString(16).toLocaleUpperCase() + greenRemainer.toString(16).toLocaleUpperCase() +
+          blueDivisor.toString(16).toLocaleUpperCase() + blueRemainer.toString(16).toLocaleUpperCase();
+
+console.log("16进制:"+hex);
